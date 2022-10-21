0b64247ff9b8d276b7d3907719bd52889410063f
leon okemba <>
2022-10-22 01:38:35 +0300

    Task_app_assignment1

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ window.addEventListener('load', () =>{
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
 
-    form.addEventListener('submit',(e) => {
+    form.addEventListener('submit',(e) => {  
         e.preventDefault();
+
+        /*Assignment - Validation code
+        Validation code so as to not enter a blank task */
+        var x = document.forms["task_form"]["new-task-input"].value;
+        if (x == "" || x == null) {
+          alert("Task must be filled out");
+          return false;
+        }
+
         const task = input.value;
         const task_el = document.createElement('div');
         task_el.classList.add('task');
 
-        const task_content_el = document.createElement('div');
+        /* Assignment - Numbered tasks
+        'Div' is replaced with 'li' so as to have numbered tasks; Also 'div' is replaced by 'ol' in html */
+        const task_content_el = document.createElement('li');
         task_content_el.classList.add('content');
 
         task_el.appendChild(task_content_el);
@@ -25,6 +36,7 @@ window.addEventListener('load', () =>{
         const task_action_el = document.createElement('div');
         task_action_el.classList.add('actions');
 
+       
         const task_edit_el = document.createElement('button');
         task_edit_el.classList.add('edit');
         task_edit_el.innerText = 'Edit';
@@ -54,10 +66,13 @@ window.addEventListener('load', () =>{
         });
         task_del_el.addEventListener('click', (e) =>{
             list_el.removeChild(task_el);
+
+            
+              
         });
     });
 });
 
-//Assignment - Set up a validation such that you cannot enter a blank task (Hint: submit add event listener)
+//Assignment - Set up a validation such that you cannot enter a blank task (Hint: submit, add event listener)
 //           - Figure out how to number the tasks
                
